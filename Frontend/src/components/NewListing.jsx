39b53b6ba7c2dca31a5c261297452a9f410013e4
randipdas <ranadipdas07@gmail.com>
2025-06-07 20:38:46 +0530

    Update

--- Frontend/src/components/NewListing.jsx
+++ Frontend/src/components/NewListing.jsx
@@ -8,13 +8,13 @@ const NewListing = () => {
          <div className="px-10 py-10 flex justify-center">
             <div className="bg-white px-10 py-10 flex flex-col gap-3 w-3/4 rounded-2xl shadow-2xl">
                <h4>Enter your home name</h4>
-              <TextField id="outlined-basic" label="Home Name" variant="outlined"/>
+              <TextField id="outlined-basic" label="Home Name" variant="outlined" size="small"/>
               <h4>Enter your home description</h4>
-              <TextField id="outlined-basic" multiline label="Home Description" variant="outlined" className="w-full"/>
+              <TextField id="outlined-basic" multiline label="Home Description" variant="outlined" size="small" className="w-full"/>
               <h4>Enter your home location</h4>
-              <TextField id="outlined-basic" label="Home Location" variant="outlined" className="w-full"/>
+              <TextField id="outlined-basic" label="Home Location" variant="outlined" size="small" className="w-full"/>
               <h4>Enter your home Price</h4>
-              <TextField id="outlined-basic" label="Price per night" variant="outlined" className="w-full" type="number"/>
+              <TextField id="outlined-basic" label="Price per night" variant="outlined" size="small" className="w-full" type="number"/>
               <button className="bg-indigo-600 text-amber-50 text-2xl self-start px-10 py-3 rounded-full transition-all ease-in-out cursor-pointer hover:scale-105">Create</button>
             </div>
          </div>
